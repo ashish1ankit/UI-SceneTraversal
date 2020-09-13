489012f2cabd62d08b159207894cc969ac64531c
ashish1ankit <32021646+ashish1ankit@users.noreply.github.com>
2020-09-13 23:10:42 +0530

    Second Commit
    Upload of Unity Project

---
+++ vr Application/Assets/ChangeScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScreen : MonoBehaviour
+{
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
---
+++ vr Application/Assets/ExitThisGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitThisGame : MonoBehaviour
+{
+    public void ExitGame()
+    {
+    	//Just to make sure its Working
+
+    	UnityEngine.Debug.LogError("Exit Game");
+    	Application.Quit();
+    	
+    }
+}
